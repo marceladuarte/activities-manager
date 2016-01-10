f9df461d7deeed01583892be3b291f59e16c22ad
Marcela Pedrini Duarte <marcela.pedrinid@gmail.com>
2016-01-10 16:35:47 -0200

    Model to MongoDB application

---
+++ model/Activity.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+
+var ActivitySchema = new mongoose.Schema({
+  summary: String,
+  description: String,
+  time: {type: Number, default: 1},
+});
+
+mongoose.model('Activity', ActivitySchema);
